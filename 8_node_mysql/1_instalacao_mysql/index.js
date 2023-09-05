c7b56ce0586c1975c2ffa3706114841ffba2e4c7
Moises Sutil <131698525+moisessutil@users.noreply.github.com>
2023-09-05 16:52:58 -0300

    novos ex

---
+++ 8_node_mysql/1_instalacao_mysql/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '15032004',
+    database: 'nodemysql2',
+})
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err)
+    }
+
+    console.log('Conectado ao MySql!')
+
+    app.listen(3000)
+})
